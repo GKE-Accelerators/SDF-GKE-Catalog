50593b990af3943f61ffff8fc8521eab099e69a0
gushob21 <gushob@google.com>
2022-12-19 23:24:31 +0000

    Adding platform blueprint

--- install/variables.tf
+++ install/variables.tf
@@ -1,55 +1,42 @@
 variable "gcp_org" {
-    default = "gushob.com"
 
 }
 
 variable "billing_account" {
-    default = "011C32-71E603-2BC9B0"
 }
 
 variable "platform_project_name" {
-    default = "test-multi-tenant "
 }
 
 variable "github_user" {
-    default = "gushob21"
 }
 
 variable "platform_repo_name" {
-    default = "test-multi-tenant-repo"
     
 }
 
 variable "github_token" {
-    default = "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"
     
 }
 variable "github_org" {
-    default = "cloud-maniac-temp"
     
 }
 
 variable "region" {
-    default = "us-central1"
 }
 
 variable "trigger_type" {
-    default = "webhook"
     
 }
 
 variable "iam_group" {
-    default = "blueprint-devops"
 }
 
 variable "folder" {
-    default = ""
 }
 
 variable "app_project_name" {
-    default = "tenant-factory"
 }
 
 variable "app_project_repo" {
-    default = "tenant-factory-repo"
 }
